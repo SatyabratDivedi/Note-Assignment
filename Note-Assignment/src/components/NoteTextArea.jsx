40baae8e197c8bb1f0d59477415013aae6cfa983
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-04-24 12:38:38 +0530

    Add new features and fix formatting in NoteTextArea component

--- Note-Assignment/src/components/NoteTextArea.jsx
+++ Note-Assignment/src/components/NoteTextArea.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './NoteTextArea.module.css';
 import { useParams } from 'react-router-dom';
+import { GoDotFill } from 'react-icons/go';
 
 function NoteTextArea() {
   const params = useParams();
@@ -9,6 +10,7 @@ function NoteTextArea() {
   return (
     <>
       <div className={style.textAreaContainer}>
+        {/* header */}
         <div className={style.headerArea}>
           <div className={style.groupSortName} style={{ background: `${mainItem[0].color}` }}>
             {mainItem[0].groupName
@@ -17,6 +19,27 @@ function NoteTextArea() {
               .join('')}
           </div>
           <div className={style.groupName}>{mainItem[0].groupName}</div>
+        </div>
+        {/* text area */}
+        <div className={style.textStoreArea}>
+          {['','','','','','',''].map(() => (
+            <div className={style.textStoreAreaBox}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita similique enim sed. Quam, reprehenderit ab aliquam veritatis natus, hic ea dolore debitis ipsa voluptatem totam quae
+              magnam assumenda tempora. Consectetur!
+              <div className={style.dateArea}>
+                {new Date().toDateString()}
+                <span style={{ transform: 'translateY(1px)' }}>
+                  <GoDotFill />
+                </span>
+                {new Date().toLocaleTimeString()}
+              </div>
+            </div>
+          ))}
+        </div>
+        {/* footer Area */}
+
+        <div className={style.footerArea}>
+          <input type="text" name="" id="" placeholder="Enter your text here......" />
         </div>
       </div>
     </>

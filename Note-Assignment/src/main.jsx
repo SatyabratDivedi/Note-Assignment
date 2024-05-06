45bb9311e637ae5dfd810123789dac9e40361e1b
Satyabrat Divedi <raghavstar1991@gmail.com>
2024-05-06 23:28:29 +0530

    chore: Remove unused imports and components

--- Note-Assignment/src/main.jsx
+++ Note-Assignment/src/main.jsx
@@ -3,13 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./App.css";
-
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import NoteTextArea from "./components/NoteTextArea.jsx";
-
 import {store} from "./reduxStore/store";
 import {Provider} from "react-redux";
-import Errorpage from "./components/ErrorPage.jsx";
 import NoteArea from "./components/NoteArea.jsx";
 
 const router = createBrowserRouter([
